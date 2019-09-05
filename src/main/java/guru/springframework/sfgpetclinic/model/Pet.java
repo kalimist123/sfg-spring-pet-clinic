62d0c4de974beb751bc35e2128acceb511728942
JackTorrence <jacktorrence420@gmx.com>
2019-09-05 19:56:15 +0100

    Adding Pojos. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    private LocalDate birthDate;
+    private Owner owner;
+    private PetType petType;
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
